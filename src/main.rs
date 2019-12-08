38d297400143d89de1dedfc9814fc766a28c9444
Emerentius <emerentius@arcor.de>
2019-12-08 13:46:59 +0100

    day 3: part 1

--- src/main.rs
+++ src/main.rs
@@ -107,12 +107,65 @@ fn run_intcode_program(mut memory: Vec<i64>) -> i64 {
     memory[0]
 }
 
+// ===============================================================================================
+//                                      Day 3
+// ===============================================================================================
+
+// in manhattan distance, a square is a circle
+const GRID_RADIUS: i32 = 10000;
+
+fn day_3(_part: Part) {
+    let input: &str = include_str!("day_3_input.txt");
+    let n_lines = input.lines().count();
+
+    // necessary size guessed, will crash if it's too little
+    // middle at (GRID_RADIUS, GRID_RADIUS)
+    // with GRID_RADIUS cells to each side
+    let mut wires_on_cell = vec![[0u8; 2*GRID_RADIUS as usize + 1]; 2*GRID_RADIUS as usize + 1];
+
+    for (wire_nr, wire) in input.lines().enumerate() {
+        assert!(wire_nr < n_lines);
+        let wire_mask = 1 << wire_nr;
+
+        let mut x = GRID_RADIUS;
+        let mut y = GRID_RADIUS;
+        for segment in wire.split(',') {
+            let (direction, steps) = segment.split_at(1);
+            let n_steps = steps.parse::<u64>().unwrap();
+            let (dx, dy) = match direction {
+                "R" => (1, 0),
+                "L" => (-1, 0),
+                "U" => (0, 1),
+                "D" => (0, -1),
+                _ => unreachable!(),
+            };
+
+            for _ in 0..n_steps {
+                x += dx;
+                y += dy;
+                wires_on_cell[y as usize][x as usize] |= wire_mask;
+            }
+        }
+    }
+
+    // inefficient, but simple
+    let distance = wires_on_cell.iter().enumerate()
+        .flat_map(|(y, row)| row.iter().enumerate().map(move |(x, wires)| (x, y, wires.count_ones())))
+        .filter(|&(_, _, n_wires)| n_wires > 1)
+        .map(|(x, y, _)| (x as i32 - GRID_RADIUS).abs() + (y as i32 - GRID_RADIUS).abs())
+        .min()
+        .unwrap();
+    println!("{}", distance);
+}
+
 fn main() {
+    // keep old code in here to avoid unused function warnings
     if false {
         day_1(Part::One);
         day_1(Part::Two);
         day_2(Part::One);
+        day_2(Part::Two);
     }
 
-    day_2(Part::Two);
+    day_3(Part::One);
 }
