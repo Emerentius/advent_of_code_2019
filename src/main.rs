0441f28fede34736034efae8151f0b28e606f39d
Emerentius <emerentius@arcor.de>
2019-12-08 11:19:27 +0100

    day 1

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+enum Part {
+    One,
+    Two,
+}
+
+// ===============================================================================================
+//                                      Day 1
+// ===============================================================================================
+fn naive_fuel_for_module(mass: i64) -> i64 {
+    mass / 3 - 2
+}
+
+fn fuel_for_module(mass: i64) -> i64 {
+    std::iter::successors(Some(mass), |&mass| Some(naive_fuel_for_module(mass)))
+        .skip(1) // module mass itself is not fuel
+        .take_while(|&fuel| fuel > 0)
+        .sum()
+}
+
+fn day_1(part: crate::Part) {
+    static INPUT: &str = include_str!("day_1_input.txt");
+    let fuel_for_module = match part {
+        Part::One => naive_fuel_for_module,
+        Part::Two => fuel_for_module,
+    };
+
+    let total_fuel = INPUT
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| line.parse::<i64>().unwrap())
+        .map(fuel_for_module)
+        .sum::<i64>();
+
+    println!("{}", total_fuel);
+}
+
+#[test]
+fn fuel_required() {
+    for &(mass, fuel) in [(12, 2), (14, 2), (1969, 654), (100756, 33583)].iter() {
+        assert_eq!(naive_fuel_for_module(mass), fuel);
+    }
+}
+
+fn main() {
+    day_1(Part::One);
+    day_1(Part::Two);
+}
